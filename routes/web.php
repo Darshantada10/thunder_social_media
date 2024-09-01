45c8569358723641356fcc53a684f615d3c93aaf
Darshantada10 <darshantadaofficial@gmail.com>
2024-09-01 14:33:00 +0530

    new project

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\Admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     // return view('Layouts.AdminApp');
+//     // return view('Layouts/AdminApp');
+// });
+
+// Route::get('/admin/brand', function () {
+//     return view('demo');
+//     // return view('Layouts/AdminApp');
+// });
+
+
+Route::get('/',[HomeController::class,'index']);
