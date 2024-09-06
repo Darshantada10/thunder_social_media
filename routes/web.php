f8d1454258b17f22a573b2c31d58d19aa3c85d98
Darshantada10 <darshantadaofficial@gmail.com>
2024-09-06 13:21:13 +0530

    new routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
-use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\FrontEnd\HomeController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/',[AuthController::class,'Login']);
+Route::get('/',[AuthController::class,'Login'])->middleware('guest')->name('login');
+Route::post('/',[AuthController::class,'AuthUser'])->name('authenticate.user');
 Route::post('/new/user/register',[AuthController::class,'SaveUser']);
+Route::get('/logout',[AuthController::class,'Logout']);
+
+
+Route::get('/home',[HomeController::class,'Index'])->middleware('auth');
