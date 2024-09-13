afe0ebef5861c7d780a1bf4ac688b070e0edb753
Darshantada10 <darshantadaofficial@gmail.com>
2024-09-13 13:11:26 +0530

    new methods

--- resources/views/FrontEnd/Home/Index.blade.php
+++ resources/views/FrontEnd/Home/Index.blade.php
@@ -4,6 +4,31 @@
 @section('Content')
     
 
+{{-- @foreach ($navbar as $nav)
+
+			@if ($nav->sub_menu[0]['title'] == null)
+
+				<li>
+					<a href="/{{$nav->url}}" title="">{{$nav->title}}</a>
+				</li>
+			
+			@else
+    
+        <li>
+            <a href="/{{$nav->url}}" title="">{{$nav->title}}</a>
+    
+            @foreach ($nav->sub_menu as $sub_menu)
+       
+                <ul>
+                    <li><a href="{{$sub_menu['url']}}" title="">{{$sub_menu['title']}}</a></li>
+                </ul>
+            @endforeach
+        </li>
+   
+    @endif
+
+@endforeach --}}
+
     <h1>Welcome to our website</h1>
 
 
