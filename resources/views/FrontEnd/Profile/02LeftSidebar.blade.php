2c2374ae23f2c81e0b46e1dd61fad36e6625c57f
Darshantada10 <darshantadaofficial@gmail.com>
2024-09-14 13:28:23 +0530

    profile sidebar left

---
+++ resources/views/FrontEnd/Profile/02LeftSidebar.blade.php
@@ -0,0 +1,54 @@
+<div class="col-lg-3">
+    <aside class="sidebar static">
+        <div class="widget">
+            <h4 class="widget-title">Recent Activity</h4>
+            <ul class="activitiez">
+                <li>
+                    <div class="activity-meta">
+                        <i>10 hours Ago</i>
+                        <span><a title="" href="#">Commented on Video posted </a></span>
+                        <h6>by <a href="time-line.html">black demon.</a></h6>
+                    </div>
+                </li>
+                <li>
+                    <div class="activity-meta">
+                        <i>30 Days Ago</i>
+                        <span><a title="" href="#">Posted your status. “Hello guys, how are you?”</a></span>
+                    </div>
+                </li>
+                <li>
+                    <div class="activity-meta">
+                        <i>2 Years Ago</i>
+                        <span><a title="" href="#">Share a video on her timeline.</a></span>
+                        <h6>"<a href="#">you are so funny mr.been.</a>"</h6>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="widget stick-widget">
+            <h4 class="widget-title">Edit info</h4>
+            <ul class="naves">
+                <li>
+                    <i class="ti-info-alt"></i>
+                    <a href="edit-profile-basic.html" title="">Basic info</a>
+                </li>
+                <li>
+                    <i class="ti-mouse-alt"></i>
+                    <a href="edit-work-eductation.html" title="">Education & Work</a>
+                </li>
+                <li>
+                    <i class="ti-heart"></i>
+                    <a href="edit-interest.html" title="">My interests</a>
+                </li>
+                <li>
+                    <i class="ti-settings"></i>
+                    <a href="edit-account-setting.html" title="">account setting</a>
+                </li>
+                <li>
+                    <i class="ti-lock"></i>
+                    <a href="edit-password.html" title="">change password</a>
+                </li>
+            </ul>
+        </div><!-- settings widget -->										
+    </aside>
+</div>
