6a4879be6cc773b1a523fa8668aa0bafa1c74468
Darshantada10 <darshantadaofficial@gmail.com>
2024-09-14 13:28:31 +0530

    profile sidebar right

---
+++ resources/views/FrontEnd/Profile/03RightSidebar.blade.php
@@ -0,0 +1,120 @@
+<div class="col-lg-3">
+    <aside class="sidebar static">
+        <div class="widget">
+                <h4 class="widget-title">Your page</h4>	
+                <div class="your-page">
+                    <figure>
+                        <a title="" href="#"><img alt="" src="images/resources/friend-avatar9.jpg"></a>
+                    </figure>
+                    <div class="page-meta">
+                        <a class="underline" title="" href="#">My page</a>
+                        <span><i class="ti-comment"></i>Messages <em>9</em></span>
+                        <span><i class="ti-bell"></i>Notifications <em>2</em></span>
+                    </div>
+                    <div class="page-likes">
+                        <ul class="nav nav-tabs likes-btn">
+                            <li class="nav-item"><a data-toggle="tab" href="#link1" class="active">likes</a></li>
+                             <li class="nav-item"><a data-toggle="tab" href="#link2" class="">views</a></li>
+                        </ul>
+                        <!-- Tab panes -->
+                        <div class="tab-content">
+                          <div id="link1" class="tab-pane active fade show">
+                            <span><i class="ti-heart"></i>884</span>
+                              <a title="weekly-likes" href="#">35 new likes this week</a>
+                              <div class="users-thumb-list">
+                                  <a data-toggle="tooltip" title="" href="#" data-original-title="Anderw">
+                                    <img alt="" src="images/resources/userlist-1.jpg">  
+                                </a>
+                                <a data-toggle="tooltip" title="" href="#" data-original-title="frank">
+                                    <img alt="" src="images/resources/userlist-2.jpg">  
+                                </a>
+                                <a data-toggle="tooltip" title="" href="#" data-original-title="Sara">
+                                    <img alt="" src="images/resources/userlist-3.jpg">  
+                                </a>
+                                <a data-toggle="tooltip" title="" href="#" data-original-title="Amy">
+                                    <img alt="" src="images/resources/userlist-4.jpg">  
+                                </a>
+                                <a data-toggle="tooltip" title="" href="#" data-original-title="Ema">
+                                    <img alt="" src="images/resources/userlist-5.jpg">  
+                                </a>
+                                <a data-toggle="tooltip" title="" href="#" data-original-title="Sophie">
+                                    <img alt="" src="images/resources/userlist-6.jpg">  
+                                </a>
+                                <a data-toggle="tooltip" title="" href="#" data-original-title="Maria">
+                                    <img alt="" src="images/resources/userlist-7.jpg">  
+                                </a>  
+                              </div>
+                          </div>
+                          <div id="link2" class="tab-pane fade">
+                              <span><i class="ti-eye"></i>445</span>
+                              <a title="weekly-likes" href="#">440 new views this week</a>
+                              <div class="users-thumb-list">
+                                  <a data-toggle="tooltip" title="" href="#" data-original-title="Anderw">
+                                    <img alt="" src="images/resources/userlist-1.jpg">  
+                                </a>
+                                <a data-toggle="tooltip" title="" href="#" data-original-title="frank">
+                                    <img alt="" src="images/resources/userlist-2.jpg">  
+                                </a>
+                                <a data-toggle="tooltip" title="" href="#" data-original-title="Sara">
+                                    <img alt="" src="images/resources/userlist-3.jpg">  
+                                </a>
+                                <a data-toggle="tooltip" title="" href="#" data-original-title="Amy">
+                                    <img alt="" src="images/resources/userlist-4.jpg">  
+                                </a>
+                                <a data-toggle="tooltip" title="" href="#" data-original-title="Ema">
+                                    <img alt="" src="images/resources/userlist-5.jpg">  
+                                </a>
+                                <a data-toggle="tooltip" title="" href="#" data-original-title="Sophie">
+                                    <img alt="" src="images/resources/userlist-6.jpg">  
+                                </a>
+                                <a data-toggle="tooltip" title="" href="#" data-original-title="Maria">
+                                    <img alt="" src="images/resources/userlist-7.jpg">  
+                                </a>  
+                              </div>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <div class="widget stick-widget">
+            <h4 class="widget-title">Who's follownig</h4>
+            <ul class="followers">
+                <li>
+                    <figure><img src="images/resources/friend-avatar2.jpg" alt=""></figure>
+                    <div class="friend-meta">
+                        <h4><a href="time-line.html" title="">Kelly Bill</a></h4>
+                        <a href="#" title="" class="underline">Add Friend</a>
+                    </div>
+                </li>
+                <li>
+                    <figure><img src="images/resources/friend-avatar4.jpg" alt=""></figure>
+                    <div class="friend-meta">
+                        <h4><a href="time-line.html" title="">Issabel</a></h4>
+                        <a href="#" title="" class="underline">Add Friend</a>
+                    </div>
+                </li>
+                <li>
+                    <figure><img src="images/resources/friend-avatar6.jpg" alt=""></figure>
+                    <div class="friend-meta">
+                        <h4><a href="time-line.html" title="">Andrew</a></h4>
+                        <a href="#" title="" class="underline">Add Friend</a>
+                    </div>
+                </li>
+                <li>
+                    <figure><img src="images/resources/friend-avatar8.jpg" alt=""></figure>
+                    <div class="friend-meta">
+                        <h4><a href="time-line.html" title="">Sophia</a></h4>
+                        <a href="#" title="" class="underline">Add Friend</a>
+                    </div>
+                </li>
+                <li>
+                    <figure><img src="images/resources/friend-avatar3.jpg" alt=""></figure>
+                    <div class="friend-meta">
+                        <h4><a href="time-line.html" title="">Allen</a></h4>
+                        <a href="#" title="" class="underline">Add Friend</a>
+                    </div>
+                </li>
+            </ul>
+        </div><!-- who's following -->
+    </aside>
+</div>
