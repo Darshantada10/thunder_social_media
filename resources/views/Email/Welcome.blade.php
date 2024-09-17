0a2c82fa2368ebbd62ddfd3e590a1cf3c567421a
Darshantada10 <darshantadaofficial@gmail.com>
2024-09-17 13:15:47 +0530

    welcome mail

---
+++ resources/views/Email/Welcome.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+
+
+    <center>
+
+        <img src="https://images.pexels.com/photos/1118869/pexels-photo-1118869.jpeg?cs=srgb&dl=pexels-jplenio-1118869.jpg&fm=jpg" alt="Thunder Image" width="50" height="50">
+
+        <h1 style="color: red" >Welcome to Thunder Social Media</h1>
+    
+    </center>
+
+    Hello {{$username}}, welcome to our website and please feel free to explore our services and features.
+    {{-- Hello Kajal, welcome to our website and please feel free to explore our services and features. --}}
+
+
+
+
+
+</body>
+</html>
