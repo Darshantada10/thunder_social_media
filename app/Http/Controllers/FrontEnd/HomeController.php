b106dba039620f872be7d973090973b9fd413d30
Darshantada10 <darshantadaofficial@gmail.com>
2024-09-28 13:29:17 +0530

    Home controller

--- app/Http/Controllers/FrontEnd/HomeController.php
+++ app/Http/Controllers/FrontEnd/HomeController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\FrontEnd;
 
-use App\Http\Controllers\Controller;
 use App\Models\Logo;
 use App\Models\Navbar;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -18,8 +19,10 @@ class HomeController extends Controller
         // $logo = Logo::where('status','=','1')->get();
         $logo = Logo::where('status','=','1')->orderBy('id','desc')->first();
         $navbar = Navbar::where('visible','1')->orderBy('id','asc')->get();
+        $userdata = Auth::user();
+        // dd($userdata);
         // dd($navbar);
         // dd($logo);
-        return view('FrontEnd.Home.Index',compact('logo','navbar'));
+        return view('FrontEnd.Home.Index',compact('logo','navbar','userdata'));
     }
 }
