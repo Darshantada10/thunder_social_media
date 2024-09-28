c630a6191ffad50c2e4e9e7cdbadf23d36200c6e
Darshantada10 <darshantadaofficial@gmail.com>
2024-09-28 13:29:26 +0530

    post Controller methods

---
+++ app/Http/Controllers/FrontEnd/PostController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\FrontEnd;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function Index(Request $request)
+    {
+        // dd($request);
+        
+
+    }
+}
