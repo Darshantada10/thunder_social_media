5437dca6b401a02604e22e7ef4af841bd71d822c
Darshantada10 <darshantadaofficial@gmail.com>
2024-09-28 13:30:24 +0530

    new table

---
+++ database/migrations/2024_09_28_075037_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+
+            $table->longText('caption')->nullable();
+            $table->string('image')->nullable();
+            $table->string('video')->nullable();
+            $table->string('music')->nullable();
+            
+            $table->foreign('user_id')->on('users')->references('id')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
