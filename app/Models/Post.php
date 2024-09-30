ffa0e388e0c5c3b1821ff48c73e10b1385ff42c9
Darshantada10 <darshantadaofficial@gmail.com>
2024-09-30 13:25:20 +0530

    model relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+    protected $fillable = ['user_id','caption','image','video','music'];
+
+    protected $casts = ['deleted_at'=>'datetime'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+        // this class(post) belong to user class
+    }
 }
