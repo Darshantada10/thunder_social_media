a5211a97c7da97215525b10dc76c75b5870972fe
Darshantada10 <darshantadaofficial@gmail.com>
2024-10-04 13:24:09 +0530

    new method

--- app/Http/Controllers/CheckController.php
+++ app/Http/Controllers/CheckController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class CheckController extends Controller
@@ -9,5 +10,28 @@ class CheckController extends Controller
     public function Index()
     {
         return view('Unauthorized');
+    }
+    public function api()
+    {
+        $data = User::all();
+        // echo $data;
+        return response()->json($data);
+    }
+    public function saveuser(Request $request)
+    {
+        // dd($request);
+        // $email = $request->email;
+        // $username = $request->username;
+        
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => $request->password,
+            'dob' => $request->dob ?? null,
+            'gender' => $request->gender ?? 'male',
+        ]);
+
+        return response()->json(['message'=>'Account Created Successfully']);
     }
 }
